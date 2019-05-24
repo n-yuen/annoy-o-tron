57776c4b3e6d9f1365c2c9545cc0b09e0c34d749
Nathan Yuen <nyuen@g.ucla.edu>
2019-05-24 03:25:29 -0700

    added README and exmaple

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {   // Play unique mp3 i
     var oldUserChannel = oldMember.voiceChannel
 
     if (oldUserChannel === undefined && newUserChannel !== undefined) {
-        if (newMember.roles.find(role => role.name === process.env.ROLE_TRIGGER)) {
+        if (newMember.roles.find(role => role.name === messages.role_trigger)) {
 
             vc = newMember.voiceChannel
 
             vc.join().then(connection => {
                 var dispatcher = connection.playFile('./bassboosted.mp3')
-                dispatcher.on("end", end => { 
+                dispatcher.on("end", end => {
                     vc.leave()
                 })
             }).catch(err => console.log(err))
@@ -28,40 +28,57 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {   // Play unique mp3 i
 })
 
 client.on('message', (message) => {     // Play mp3 if special message is sent in chat in your voice channel,
-                                        // or in the voice channel of someone you pinged
+    // or in the voice channel of someone you pinged
     var member = message.member
     var content = message.content
 
     if (content.startsWith('!')) {
 
         var mention = message.mentions.members.first()
-        if (mention !== undefined){
+        if (mention !== undefined) {
             member = mention
         }
         vc = member.voiceChannel
 
         var toPlay = undefined
-        for ( var i = 0; i < messages.length; i++ ) {
-            var m = messages[i]
-            if (content.startsWith(m.trigger)){
-                toPlay = './audio/' + m.file
-                break
+        if (content == '!null') {
+            joinLeave(10)
+            message.delete()
+        } else {
+            for (var i = 0; i < messages.sound_triggers.length; i++) {
+                var m = messages.sound_triggers[i]
+                if (content.startsWith(m.trigger)) {
+                    toPlay = './audio/' + m.file
+                    break
+                }
+            }
+            if (toPlay === undefined) {
+                return
+            }
+
+            if (vc !== undefined) {
+                vc.join().then(connection => {
+                    var dispatcher = connection.playFile(toPlay)
+                    dispatcher.on("end", end => {
+                        vc.leave()
+                    })
+                }).catch(err => console.log(err))
+                message.delete()
             }
         }
-        if (toPlay === undefined){
-            return
-        }
-
-        if (vc !== undefined){
-            vc.join().then(connection => {
-                var dispatcher = connection.playFile(toPlay)
-                dispatcher.on("end", end => { 
-                    vc.leave()
-                })
-            }).catch(err => console.log(err))
-            message.delete()
-        }
     }
-  })
+})
+
+function joinLeave(i) {
+    setTimeout(function () {
+        vc.join().then(connection => {
+            setTimeout(function () {
+                vc.leave()
+                if (--i) (joinLeave(i))
+            }, 500)
+        }).catch(err => console.log(err))
+    }, 500)
+
+}
 
 client.login(process.env.BOT_TOKEN)

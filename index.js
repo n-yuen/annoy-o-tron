b71466b626029d3215a897e014f42d9402ff721b
Nathan Yuen <nyuen@g.ucla.edu>
2019-05-25 23:55:37 -0700

    added text spam feature. changed command syntax

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const messages = require('./messages.json')
 
-var vc = undefined;
+var vc = undefined  // Voice channel that the bot will be joining
 
 function leave() {
     vc.leave()
-    vc = undefined;
+    vc = undefined
 }
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {   // Play unique mp3 if user with role enters the chat
@@ -20,17 +20,23 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {   // Play unique mp3 i
     var oldUserChannel = oldMember.voiceChannel
 
     if (oldUserChannel === undefined && newUserChannel !== undefined) { // user joined channel
+        var song = undefined
         if (newMember.roles.find(role => role.name === messages.role_trigger)) {
-
-            vc = newMember.voiceChannel
-
-            vc.join().then(connection => {
-                var dispatcher = connection.playFile('./audio/bassboosted.mp3')
-                dispatcher.on("end", end => {
-                    leave()
-                })
-            }).catch(err => console.log(err))
+            song = 'bassboosted.mp3'
+        } else {
+            song = 'bruh.mp3'   // Plays when user without the role enters the chat. Comment out to avoid some cancer
         }
+
+        vc = newMember.voiceChannel
+
+        vc.join().then(connection => {
+            var dispatcher = connection.playFile('./audio/' + song)
+            dispatcher.on("end", end => {
+                leave()
+            })
+        }).catch(err => console.log(err))
+
+
     } else if (newUserChannel === undefined && vc !== undefined) {  // user left channel
         leave()
     }
@@ -40,12 +46,69 @@ client.on('message', (message) => {     /*
                                         * Play mp3 if special message is sent in chat in your voice channel,
                                         * or in the voice channel of someone you pinged
                                         */
-    var member = message.member
     var content = message.content
 
-    var expected_len = 2
-
     if (content.startsWith('!')) {
+
+        var iter        // Number of times the command is to be executed, in a row
+
+        var isSpam = content.startsWith('!spam')
+        var splitIndex = content.indexOf(' ')
+        var firstWord
+        var remainder = undefined
+        var multiWord = (splitIndex === -1)
+
+        if (multiWord){ 
+            if (isSpam)
+                return
+            firstWord = content
+        } else {        // Parse into multiple words
+            firstWord = content.substr(0, splitIndex)
+            remainder = content.substr(splitIndex + 1)
+        }
+
+        var iter_default = 1    // Default iter value for non-spam command
+        if (isSpam)
+            iter_default = 20   // Default iter value for spam command
+
+        var fWArr = firstWord.split('=')    // Parse first word by looking for repeat command
+        var fWLen = fWArr.length
+        if (fWLen > 2)
+            return
+        var fWData = fWArr[0]       // Before equals sign
+        var fWNum = fWArr[1]        // After equals sign
+
+        if (fWLen === 1) {
+            iter = iter_default
+        } else if (!isNaN(fWNum)) {
+            iter = fWNum
+        } else {
+            return
+        }
+
+        if (isSpam) {       // Text spam
+
+            console.log(member.user.tag + ': ' + content)
+
+            iter = Math.min(iter, 50) // prevent cancer
+
+            function sendMessage(i) {   // Repeatedly send text message
+                message.channel.send(remainder)
+                setTimeout(() => { if (--i) (sendMessage(i)) }, 2000)
+            }
+
+            sendMessage(iter)
+            message.delete()
+
+            return
+        }
+
+        var member = message.member
+
+        var expected_len = 0
+        var numWords = 0    // used to check correct syntax
+        if (remainder !== undefined)
+            numWords = remainder.split(" ").length
 
         var mention = message.mentions.members.first()  // check for pinged member
         if (mention !== undefined) {
@@ -54,26 +117,17 @@ client.on('message', (message) => {     /*
         }
 
         vc = member.voiceChannel
-        if (vc === undefined)
+        if (vc === undefined || numWords > expected_len)
             return
-
-        var parsed_content = content.split(" ")
-        if (parsed_content.length > expected_len)
-            return
-
-        var iter = 0 // number of times to repeat the action
-        if (parsed_content.length > expected_len - 1) { // read number as second word of the message, if applicable, and store to iter
-            var content2 = parsed_content[1]
-            if (content2 !== undefined && !isNaN(content2))
-                iter = parseInt(content2, 10)
-        }
 
         iter = Math.min(iter, 20)   // avoid utter cancer
 
-        var toPlay = undefined
-        if (parsed_content[0] == '!harass') {
+        console.log('we are here')
+        console.log(fWData)
 
-            console.log(member.nickname + ': ' + content)
+        if (fWData == '!harass') {
+
+            console.log(member.user.tag + ': ' + content)
 
             if (iter === 0) iter = 10
 
@@ -91,9 +145,11 @@ client.on('message', (message) => {     /*
             joinLeave(iter)
 
         } else {
+            var toPlay = undefined
             for (var i = 0; i < messages.sound_triggers.length; i++) {  // look for correct song to play
                 var m = messages.sound_triggers[i]
-                if (parsed_content[0] == m.trigger) {
+                
+                if (fWData == m.trigger) {
                     toPlay = './audio/' + m.file
                     break
                 }
@@ -102,7 +158,7 @@ client.on('message', (message) => {     /*
                 return
             }
 
-            console.log(member.nickname + ': ' + content)
+            console.log(member.user.tag + ': ' + content)
 
             if (!iter) iter++
 
